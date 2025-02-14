8676a192f7c5fc88e262795cb55181653eeffe48
BatemanOJ <ollie.j.bateman@gmail.com>
2025-02-14 16:26:44 +0000

    14-02-2025 Minor edits to bandpass and add_noise

--- bandpass_filter.py
+++ bandpass_filter.py
@@ -1,10 +1,11 @@
 from scipy.signal import butter, filtfilt # Bandpass filter
 
 # Bandpass filter: imports the data to be filtered and the filter parameters: lowcut, highcut and sampling rate
-def bandpass_filter(data, lowcut, highcut, fs, order=4):
+def bandpass_filter(data, lowcut_1, highcut_1, fs, order=4):
     nyquist = 0.5 * fs
-    low = lowcut / nyquist
-    high = highcut / nyquist
+    low = lowcut_1 / nyquist
+    high = highcut_1 / nyquist
     b, a = butter(order, [low, high], btype='band')
     filtered_data = filtfilt(b, a, data)
+
     return filtered_data
